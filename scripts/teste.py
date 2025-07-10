b50df175402dbf8a701d6532c456a7699a1e4dd3
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2025-07-10 15:15:36 -0300

    Calculo de metricas separadas por veiculo, implementado, porem ainda nao funcional

---
+++ scripts/teste.py
@@ -0,0 +1,16 @@
+# teste = {
+#     "str1": "str2",
+#     "str3": "str4",
+# }
+
+# print(len(teste))
+
+listas = [1,2,3] * 3
+print(type(listas))
+# print(listas)
+
+for lista in listas:
+    print(lista)
+
+a, b, c = 1, 1, 1
+print(a == b == c)
--- simulation.py
+++ simulation.py
@@ -15,7 +15,7 @@ TRACES_PATH = {
     "monaco"      : "MoSTScenario/scenario/most",
 }
 
-TRACE_NAME              = "santa_tereza"
+TRACE_NAME              = "luxemburg"
 SUMOCFG_FILE            = f"traces/{TRACES_PATH[TRACE_NAME]}.sumocfg"
 BOUNDS                  = utils.get_simulation_bounds(SUMOCFG_FILE)
 END_TIME                = utils.get_end_time(SUMOCFG_FILE)
@@ -27,37 +27,71 @@ DEBUGGING               = False
 SAVE_RESULTS            = True
 GET_METRICS             = True
 
+VEICULOS = {
+    "bus": "AVL",
+    "car": "car",
+}
+
 def main():
 
-    traci.start([SUMO_CONFIG, "-c", SUMOCFG_FILE])
+    step, csv_data, geoPosAPs = run_simulation()
 
-    step, csv_data, geoPosAPs = 0, [], []
+    if SAVE_RESULTS:
+        save_results(csv_data, geoPosAPs, step)
+
+def run_simulation():
+
+    traci.start([SUMO_CONFIG, "-c", SUMOCFG_FILE])
+    step, csv_data, geoPosAPs = 0, [[]] * (len(VEICULOS) + 1), []
 
     while step <= END_TIME and sim.getMinExpectedNumber() > 0:
 
         traci.simulationStep()
         step = sim.getTime()
 
-        if step % METRICS_EVERY_N_SECONDS != 0: continue
-
-        if GET_METRICS:
-            positions = utils.get_vehicle_positions()
-            G = build_graph(positions, DISTANCE_THRESHOLD, use_quadTree = (True, BOUNDS))
-
-            # Salvar estatísticas
-            metrics, coordenates = calcular_metricas(G, positions, multithreaded = False)
-            geoPosAPs.append(coordenates)
-            csv_data.append([m for m in metrics.values() if type(m) != list])
-
-            if DEBUGGING: utils.debug_stats(G, step, metrics)
+        if GET_METRICS and step % METRICS_EVERY_N_SECONDS == 0:
+            step_metrics(step, csv_data, geoPosAPs)
 
     traci.close()
 
-    if SAVE_RESULTS:
-        outputPath = f"output/simulation_{utils.getNextSimID()}_{TRACE_NAME}_{(step - 1):.0f}s_{DISTANCE_THRESHOLD}m/"
-        os.makedirs(outputPath, exist_ok = True)
+    return step, csv_data, geoPosAPs
 
-        vis.generate_histograms(csv_data, outputPath)
-        vis.generate_heat_map(geoPosAPs, outputPath)
+def step_metrics(step, csv_data, geoPosAPs):
+
+    positions = utils.get_vehicle_positions()
+    print(f"positions: {positions}")
+    G = build_graph(positions, DISTANCE_THRESHOLD, use_quadTree = (True, BOUNDS))
+    print(f"G: {G}")
+
+    # Salvar estatísticas
+    metrics_list, coordenates = calcular_metricas(VEICULOS, G, positions, multithreaded = False)
+    geoPosAPs.append(coordenates)
+
+    print(f"metrics_list: {metrics_list}")
+    print(f"geoPosAPs: {geoPosAPs}")
+
+    for i in range(len(metrics_list)):
+        csv_data[i].append([m for m in metrics_list[i].values() if type(m) != list])
+
+    if DEBUGGING:
+        for metrics in metrics_list:
+            utils.debug_stats(G, step, metrics)
+
+def save_results(csv_data, geoPosAPs, step):
+
+    outputPath = f"output/simulation_{utils.getNextSimID()}_{TRACE_NAME}_{(step - 1):.0f}s_{DISTANCE_THRESHOLD}m/"
+    os.makedirs(outputPath, exist_ok = True)
+
+    vis.generate_histograms(csv_data[0], outputPath)
+    vis.generate_heat_map(geoPosAPs, outputPath)
+
+    index = 1
+    for key in VEICULOS:
+
+        outputPathVeiculo = outputPath + key + "/"
+        os.makedirs(outputPathVeiculo, exist_ok = True)
+
+        vis.generate_histograms(csv_data[index], outputPathVeiculo)
+        index += 1
 
 if __name__ == "__main__": main()
